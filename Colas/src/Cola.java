55262e5532e0ae97775a47162f46195f10979fe6
Readyforcode <aaabeeelooon@gmail.com>
2018-05-14 12:36:32 -0400

    añadiendo Colas

---
+++ Colas/src/Cola.java
@@ -0,0 +1,44 @@
+public class Cola {
+    int frente, fin;
+    int []elem;
+    final int MAX = 50;
+    public Cola()
+    {
+        frente  = fin = MAX;
+        elem = new int[MAX+1];
+    }
+
+    public void inicializar()
+    {
+        frente = fin = MAX;
+    }
+
+    public boolean colaVacia()
+    {
+        return frente == fin;
+    }
+
+    public boolean colaLlena()
+    {
+        int sigfin;
+        if(fin == MAX)  sigfin=1;
+        else    sigfin = fin+1;
+
+        return sigfin == frente;
+    }
+
+    public int ver()
+    {
+        int sigfrente;
+        if(frente == MAX)   sigfrente = 1;
+        else sigfrente = frente+1;
+
+        return elem[sigfrente];
+    }
+
+    public void eliminar()
+    {
+        if(frente == MAX)   frente = 1;
+        else frente = frente + 1;
+    }
+}
